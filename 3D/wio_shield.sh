d108c0ecc40034f40fb1cdf68722d0afdfc61b60
julienrat <julienrat@riseup.net>
2016-08-11 09:00:36 +0200

    - ‘3D/wio_shield.pov’ - ‘3D/wio_shield.sh’

--- 3D/wio_shield.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-"povray" +W800 +H600 +A0.3 +FN +L"/usr/share/fonts/" +L"/home/julo/Developpement/eagle3D/eagle3d/ulp//../povray" +O"/home/julo/eagle/wio_shield/3D/wio_shield.png" /home/julo/eagle/wio_shield/3D/wio_shield.pov 
